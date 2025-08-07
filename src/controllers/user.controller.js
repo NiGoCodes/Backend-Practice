898188b64922ec5a97443b64b7d706666fdd317d
NiGoCodes <169056827+NiGoCodes@users.noreply.github.com>
2025-08-07 23:26:36 +0530

    Updated userController and add logics in it Also added middlewares in user Routes to upload avatar and cover

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,70 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
+import { ApiError } from '../utils/ApiError.js';
+import {User} from "../models/user.models.js"
+import { ApiResponse } from '../utils/ApiResponse.js';
+import {uploadOnCloudinary} from '../utils/cloudinary.js'
 
 const registerUser = asyncHandler( async (req , res , next) =>{
-    return res.status(200).json({
-        message:"ok"
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists : username , email
+    // check for images and avatars
+    // upload them to cloudinary & check if successfully uploaded
+    // create user object - create entry in db
+    // remove password and refreshToken from response
+    // check for user creation 
+    // return res
+
+     const { username , email  , password , fullName} = req.body
+
+     if(
+        [username , email  , password , fullName].some((field) => field?.trim() === "" )
+     ){
+        throw new ApiError(400 , "All fields are required");
+     }
+
+     const existedUser = await User.findOne({
+        $or :[{username} , {email}] 
     })
+
+    if(existedUser){
+        throw new ApiError(409, "User already exists");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    
+    if(!avatarLocalPath){
+        throw new ApiError(400 , "Avatar is required")
+    }
+    
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar || !avatar.url) {
+        throw new ApiError(400, "Avatar upload failed");
+    }
+
+    const user = await User.create({
+        username : username.toLowerCase(),
+        fullName, 
+        email , 
+        password,
+        avatar : avatar.url,
+        coverImage : coverImage?.url || "",
+    })
+
+    const createdUser = await User.findById(user._id).select("-password -refreshToken")
+
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(201 ,createdUser , "User is Successfully registered")
+    )
+
+
 })
 
 export {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,18 @@ import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    upload.fields([
+        { 
+            name: "coverImage",
+            maxCount : 1
+        },
+        {
+            name : "avatar",
+            maxCount : 1
+        }
+    ])
+    , registerUser
+);
 
 export default router;
